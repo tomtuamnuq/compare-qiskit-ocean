4f9fab6ea970bff8bc7bbef2f57c00211e878e27
tomtuamnuq <tom.krueger@fernuni-hagen.de>
2021-03-04 09:57:57 +0100

    Adding Sparse Tests up to 50 variables

--- DWave_Qiskit_Plugin_Test/lp_random_gen.py
+++ DWave_Qiskit_Plugin_Test/lp_random_gen.py
@@ -7,6 +7,8 @@ Created on Sun Jan 31 09:58:32 2021
 import numpy as np
 from qiskit.optimization import QuadraticProgram
 
+varname = lambda k,j : 'x'+str(k)+"_"+str(j)
+
 class random_lp_int:
     def __init__(self,num_constr,num_vars,lowerbound,upperbound):
         self.m = num_constr 
@@ -14,13 +16,29 @@ class random_lp_int:
         self.lb = lowerbound
         self.ub = upperbound
         
-    def createLP(self,name):
+    def createLP(self, name, multiple = 1):
         self.name = name
-        self._create_data(d_ub = self.ub+1)
-        self._createLP()
+        self.model = QuadraticProgram(self.name)
+        self.multiple = multiple
+        objective = np.array([])
+        for k in range(multiple):
+            # add variables
+            for j in range(self.n):
+                if self.lb == 0 and self.ub == 1 :
+                    self.model.binary_var(varname(k,j))
+                else:
+                    self.model.integer_var(lowerbound = self.lb, upperbound = self.ub, name = varname(k,j))
+            
+            self._create_data(d_ub = self.ub+1)
+            objective = np.append(objective,self.c)
+            self._add_constrs(k)
+            
+        # add linear objective function
+        
+        self.model.minimize(linear = objective)
         return self.model
 
-
+    
     def _create_data(self, d_ub = 1 , A_lb = -10, A_ub = 10, c_lb = -1, c_ub = 1):
         self.x = np.random.randint(self.lb,self.ub+1,size = self.n)
         self.d = np.random.randint(0,d_ub+1,size = self.m)
@@ -28,20 +46,16 @@ class random_lp_int:
         self.b = self.A.dot(self.x) + self.d
         self.c = np.random.randint(c_lb,c_ub+1,size = self.n)
         
-    def _createLP(self):
-        model = QuadraticProgram(self.name)
-
-        # add variables
-        for j in range(self.n):
-            if self.lb == 0 and self.ub == 1 :
-                model.binary_var('x'+str(j))
-            else:
-                model.integer_var(lowerbound = self.lb, upperbound = self.ub, name = 'x'+str(j))
+    
+    def _add_constrs(self,k):    
 
         # add linear constraints
         for i in range(self.m):
-            model.linear_constraint(linear=self.A[i], sense='<=', rhs=self.b[i], name='A'+str(i)+'le'+'b'+str(i))
+            linear = {}
+            for j in range(self.n):
+                linear[varname(k,j)] = self.A[i,j]
+            self.model.linear_constraint(linear=linear, sense='<=', rhs=self.b[i], name='A'+str(k)+"_le"+'b'+str(i))
+    
 
-        # add linear objective function
-        model.minimize(linear = self.c)
-        self.model = model
+        
+
